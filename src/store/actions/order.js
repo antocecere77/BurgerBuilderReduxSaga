fdd5ee99b84bf11029325e0476e34b1c4296fcb7
antocecere <antocecere77@gmail.com>
2020-04-25 18:46:24 +0200

    Fetching orders via redux

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -40,4 +40,44 @@ export const purchaseInit = () => {
     return {
         type: actionTypes.PURCHASE_INIT
     };
+};
+
+export const fetchOrdersSuccess = (orders) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    };
+};
+
+export const fetchOrderFail = (error) => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAIL,
+        error: error
+    };
+};
+
+export const fetchOrderStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    };
+};
+
+export const fecthOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrderStart());
+        axios.get('/orders.json')
+        .then(res => {
+            const fetchedOrders = [];
+            for(let key in res.data) {
+                fetchedOrders.push({
+                    ...res.data[key],
+                    id: key
+                });
+            }
+            dispatch(fetchOrdersSuccess(fetchedOrders));
+        })
+        .catch(err => {
+            dispatch(fetchOrderFail(err));
+        });
+    }
 }
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -30,10 +30,26 @@ const reducer = (state = initialState, action) => {
                 loading: false                
             };
         case actionTypes.PURCHASE_INIT: 
-        return {
-            ...state,
-            purchased: false
-        }
+            return {
+                ...state,
+                purchased: false
+            }
+        case actionTypes.FETCH_ORDERS_START: 
+            return {
+                ...state,
+                loading: true
+            } 
+        case actionTypes.FETCH_ORDERS_SUCCESS: 
+            return {
+                ...state,
+                orders: action.orders,
+                loading: false
+            }
+        case actionTypes.FETCH_ORDERS_FAIL:
+            return {
+                ...state,
+                loading: false
+            }    
         default:
             return state;        
     }
