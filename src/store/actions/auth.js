15cb91630368b836f279b21ef9c125c4ca26fd5c
antocecere <antocecere77@gmail.com>
2020-04-26 12:50:28 +0200

    Adding sign-in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,7 +22,7 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignUp) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,7 +30,11 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         }
-        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBu4xWEOY_EPOVFAsdpX3FDbhdqLN4GkbU', authData)
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBu4xWEOY_EPOVFAsdpX3FDbhdqLN4GkbU';
+        if(!isSignUp) {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBu4xWEOY_EPOVFAsdpX3FDbhdqLN4GkbU'
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data))
