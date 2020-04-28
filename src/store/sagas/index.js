579f98ebd3e143f26b5ccd0a01afc46b8bf7c1f9
antocecere <antocecere77@gmail.com>
2020-04-28 17:38:35 +0200

    Moving more logic into sagas

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,8 +1,9 @@
 import { takeEvery } from 'redux-saga/effects';
 
 import * as actionType from '../actions/actionTypes';
-import { logoutSaga } from './auth';
+import { logoutSaga, checkAuthTimeoutSaga } from './auth';
 
 export function* watchAuth() {
+    yield takeEvery(actionType.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
     yield takeEvery(actionType.AUTH_INITIATE_LOGOUT, logoutSaga);
 }
